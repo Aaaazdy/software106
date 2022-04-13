80fe8d8f51def0cafc20ac269d9f97918046df9f
Aaaazdy <1169400012@qq.com>
2022-04-13 11:39:01 +0800

    Revert "2022/4/13"
    This reverts commit 8edbc1c225520884e681e6969de0cd2dd71b7fd3.

--- SoftwareEngineer_Airline/src/Test/LunchPage.java
+++ SoftwareEngineer_Airline/src/Test/LunchPage.java
@@ -1,12 +1,10 @@
 package Test;
-import Boundary.Confirmation;
 import Boundary.Creditcard;
-import Boundary.MealSelect;
 import Entity.BookingInformation;
 
 public class LunchPage {
 	public static void main(String[] args) {
 		BookingInformation c1=new BookingInformation();
-		new MealSelect(c1).setVisible(true);//construct the first page.
+		new Creditcard(c1).setVisible(true);//construct the first page.
 	}
 }

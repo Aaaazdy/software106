e08436246f1efd9b1df13022545a191377144dc2
Aaaazdy <1169400012@qq.com>
2022-04-13 11:41:04 +0800

    2022/4/13/2

--- SoftwareEngineer_Airline/src/Entity/BookingInformation.java
+++ SoftwareEngineer_Airline/src/Entity/BookingInformation.java
@@ -2,104 +2,166 @@ package Entity;
 
 import java.util.LinkedList;
 
-public class BookingInformation {
+public class BookingInformation
+{
 
-	private String surName;
-	private String familyName;	
-	private String flightNumber;
-	private String boardingTime;
-	private String boardingGate;
-	private String seat;
-	private String primaryFood;
-	private String credFirst;
-	private String credSecond;
-	private String credNumber;
-	private String securCode;
-	private LinkedList<String> extraService=new LinkedList<String>();	
+    private String firstName;
+    private String lastName;
+    private String flightNumber;
+    private String boardingTime;
+    private String boardingGate;
+    private String seat;
+    private String primaryFood;
+    private String credFirst;
+    private String credSecond;
+    private String credNumber;
+    private String securCode;
+    private int extraServiceFee;
+    private LinkedList<String> extraService = new LinkedList<String>();
 
-	public BookingInformation() {
-		extraService.add("coca cola");
-		extraService.add("Extra legroom");
-		surName="surName";
-		familyName="familyName";
-		flightNumber="AC1234";
-		boardingGate="20";
-		seat="12A";
-		primaryFood="box lunch";
-	}
-	
-	public String getSurName() {
-		return surName;
-	}
-	public void setSurName(String surName) {
-		this.surName = surName;
-	}
-	public String getFamilyName() {
-		return familyName;
-	}
-	public void setFamilyName(String familyName) {
-		this.familyName = familyName;
-	}
+    public BookingInformation()
+    {
+        extraService.add("coca cola");
+        extraService.add("Extra legroom");
+        firstName = "firstName";
+        lastName = "lastName";
+        flightNumber = "AC1234";
+        boardingGate = "20";
+        seat = "12A";
+        primaryFood = "box lunch";
+    }
 
-	public String getFlightNumber() {
-		return flightNumber;
-	}
-	public void setFlightNumber(String flightNumber) {
-		this.flightNumber = flightNumber;
-	}
-	public String getBoardingTime() {
-		return boardingTime;
-	}
-	public void setBoardingTime(String boardingTime) {
-		this.boardingTime = boardingTime;
-	}
-	public String getBoardingGate() {
-		return boardingGate;
-	}
-	public void setBoardingGate(String boardingGate) {
-		this.boardingGate = boardingGate;
-	}
-	public String getSeat() {
-		return seat;
-	}
-	public void setSeat(String seat) {
-		this.seat = seat;
-	}
-	public String getPrimaryFood() {
-		return primaryFood;
-	}
-	public void setPrimaryFood(String primaryFood) {
-		this.primaryFood = primaryFood;
-	}
-	public String getCredFirst() {
-		return credFirst;
-	}
-	public void setCredFirst(String credFirst) {
-		this.credFirst = credFirst;
-	}
-	public String getCredSecond() {
-		return credSecond;
-	}
-	public void setCredSecond(String credSecond) {
-		this.credSecond = credSecond;
-	}
-	public String getCredNumber() {
-		return credNumber;
-	}
-	public void setCredNumber(String credNumber) {
-		this.credNumber = credNumber;
-	}
-	public String getSecurCode() {
-		return securCode;
-	}
-	public void setSecurCode(String securCode) {
-		this.securCode = securCode;
-	}
-	public String getExtraService() {
-		String retString="";
-		for(String x:this.extraService) {
-			retString+=(x+",");
-		}
-		return retString;
-	}
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+
+    public String getFlightNumber()
+    {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber)
+    {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getBoardingTime()
+    {
+        return boardingTime;
+    }
+
+    public void setBoardingTime(String boardingTime)
+    {
+        this.boardingTime = boardingTime;
+    }
+
+    public String getBoardingGate()
+    {
+        return boardingGate;
+    }
+
+    public void setBoardingGate(String boardingGate)
+    {
+        this.boardingGate = boardingGate;
+    }
+
+    public String getSeat()
+    {
+        return seat;
+    }
+
+    public void setSeat(String seat)
+    {
+        this.seat = seat;
+    }
+
+    public String getPrimaryFood()
+    {
+        return primaryFood;
+    }
+
+    public void setPrimaryFood(String primaryFood)
+    {
+        this.primaryFood = primaryFood;
+    }
+
+    public String getCredFirst()
+    {
+        return credFirst;
+    }
+
+    public void setCredFirst(String credFirst)
+    {
+        this.credFirst = credFirst;
+    }
+
+    public String getCredSecond()
+    {
+        return credSecond;
+    }
+
+    public void setCredSecond(String credSecond)
+    {
+        this.credSecond = credSecond;
+    }
+
+    public String getCredNumber()
+    {
+        return credNumber;
+    }
+
+    public void setCredNumber(String credNumber)
+    {
+        this.credNumber = credNumber;
+    }
+
+    public String getSecurCode()
+    {
+        return securCode;
+    }
+
+    public void setSecurCode(String securCode)
+    {
+        this.securCode = securCode;
+    }
+
+    public void setExtraServiceFee(int a)
+    {
+        extraServiceFee=a;
+    }
+    public int getExtraServiceFee()
+    {
+        return extraServiceFee;
+    }
+    public void setExtraService(String item)
+    {
+        extraService.add(item);
+    }
+
+    public String getExtraService()
+    {
+        String retString = "";
+        for (String x : this.extraService)
+        {
+            retString += (x + ",");
+        }
+        return retString;
+    }
 }

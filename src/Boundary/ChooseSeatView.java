e98b976079617461270ca29b6314a8d5f2f649a1
Aaaazdy <1169400012@qq.com>
2022-05-28 00:30:55 +0800

    2022/5/27 加入行号

--- src/Boundary/ChooseSeatView.java
+++ src/Boundary/ChooseSeatView.java
@@ -10,7 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
-public class ChooseSeatView extends JPanel {
+public class ChooseSeatView extends JPanel
+{
     JPanel leftpanel;
     JPanel midpanel;
     JPanel rightpanel;
@@ -29,12 +30,14 @@ public class ChooseSeatView extends JPanel {
     public JButton button2;//back button
     private List<ActionListener> listeners;
 
-    public void addActionListener(ActionListener actionlistener) {
+    public void addActionListener(ActionListener actionlistener)
+    {
         listeners.add(actionlistener);
     }
 
-    public ChooseSeatView(int[] seatInf,int planemodel) {
-        a=seatInf;
+    public ChooseSeatView(int[] seatInf, int planemodel)
+    {
+        a = seatInf;
         whiteicon = new ImageIcon("./pictures/Seat.jpg");
         redicon = new ImageIcon("./pictures/Seat(selected).jpg");
         greenicon = new ImageIcon("./pictures/Seat(green).jpg");
@@ -45,13 +48,13 @@ public class ChooseSeatView extends JPanel {
         rightpanel = new JPanel();
         uppanel = new JPanel();
         downpanel = new JPanel();
-        if (planemodel == 1) {
+        if (planemodel == 1)
+        {
             // here is the plane planemodel[1]
 
 
-
-            leftpanel.setLayout(new GridLayout(0, 3));
-            rightpanel.setLayout(new GridLayout(0, 3));
+            leftpanel.setLayout(new GridLayout(0, 4));
+            rightpanel.setLayout(new GridLayout(0, 4));
             leftpanel.setPreferredSize(new Dimension(180, 150));
             rightpanel.setPreferredSize(new Dimension(180, 150));
             midpanel.setLayout(new GridLayout(0, 1));
@@ -69,13 +72,15 @@ public class ChooseSeatView extends JPanel {
             midpanel.add(aisle);
             midpanel.add(planemodeltext);
 
-            uppanel.setLayout(new GridLayout(0, 8)); // 8 columns
+            uppanel.setLayout(new GridLayout(0, 10)); // 8 columns
             uppanel.setPreferredSize(new Dimension(0, 60));
-            JLabel uplabel[] = new JLabel[8]; // A B C D E F
+            JLabel uplabel[] = new JLabel[10]; // A B C D E F
             int aisie = 0;
-            for (int i = 0; i < 8; i++) {
-                if (i == 3 || i == 4) {
-                    uplabel[i] = new JLabel("     ", JLabel.CENTER);
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 3 || i == 4 || i == 5 || i == 6)
+                {
+                    uplabel[i] = new JLabel("               ", JLabel.CENTER);
                     uppanel.add(uplabel[i]);
                     aisie++;
                     continue;
@@ -92,16 +97,22 @@ public class ChooseSeatView extends JPanel {
             button2 = new JButton("Next");
             button1.setForeground(Color.black);
             button2.setForeground(Color.black);
-            button1.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    for (int i = 0; i < listeners.size(); ++i) {
+            button1.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    for (int i = 0; i < listeners.size(); ++i)
+                    {
                         listeners.get(i).actionPerformed(e);
                     }
                 }
             });
-            button2.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    for (int i = 0; i < listeners.size(); ++i) {
+            button2.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    for (int i = 0; i < listeners.size(); ++i)
+                    {
                         listeners.get(i).actionPerformed(e);
                     }
                 }
@@ -110,49 +121,89 @@ public class ChooseSeatView extends JPanel {
             downpanel.add(button1);
             downpanel.add(button2);
 
-            // add seat button on leftpanel and right panel
+            // add seat button on the left panel and the right panel
 
-            for (int i = 1; i <= 30; i++) {
-                if (a[i] == 0)
-                    bt[i] = new JButton(whiteicon); // 空闲位置是white
-                else
-                    bt[i] = new JButton(redicon); // 被占位置是red
+            int i1, line1 = 0;
+            // here add one more column to set as the line index
+            for (int index = 1; index <= 40; index++)
+            {
 
-                bt[i].addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        for (int i = 0; i < listeners.size(); ++i) {
-                            listeners.get(i).actionPerformed(e);
+                if (index % 4 == 0)
+                {
+                    line1++;
+                    JButton j = new JButton(String.valueOf(line1));
+                    leftpanel.add(j);
+                    j.setMargin(new Insets(0, 0, 0, 0));
+                    j.setEnabled(false);
+                    j.setBackground(Color.lightGray);
+                } else
+                {
+                    i1 = index - line1;
+                    if (a[i1] == 0)
+                        bt[i1] = new JButton(whiteicon); // 空闲位置是white
+                    else
+                        bt[i1] = new JButton(redicon); // 被占位置是red
+
+                    bt[i1].addActionListener(new ActionListener()
+                    {
+                        public void actionPerformed(ActionEvent e)
+                        {
+                            for (int i = 0; i < listeners.size(); ++i)
+                            {
+                                listeners.get(i).actionPerformed(e);
+                            }
                         }
-                    }
-                });
-                leftpanel.add(bt[i]);
-            }
-            for (int i = 31; i <= 60; i++) {
-                if (a[i] == 0)
-                    bt[i] = new JButton(whiteicon);
-                else
-                    bt[i] = new JButton(redicon);
+                    });
+                    leftpanel.add(bt[i1]);
+                    //here add the seat to the pannel
 
-                bt[i].addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        for (int i = 0; i < listeners.size(); ++i) {
-                            listeners.get(i).actionPerformed(e);
+                }
+            }
+            int i2, line2 = 0;
+            for (int index = 31; index <= 70; index++)
+            {
+                if ((index + 1) % 4 == 0)
+                {
+                    line2++;
+                    JButton j = new JButton(String.valueOf(line2));
+                    rightpanel.add(j);
+                    j.setMargin(new Insets(0, 0, 0, 0));
+                    j.setEnabled(false);
+                    j.setBackground(Color.lightGray);
+                } else
+                {
+                    i2 = index - line2;
+                    if (a[i2] == 0)
+                        bt[i2] = new JButton(whiteicon);
+                    else
+                        bt[i2] = new JButton(redicon);
+
+                    bt[i2].addActionListener(new ActionListener()
+                    {
+                        public void actionPerformed(ActionEvent e)
+                        {
+                            for (int i = 0; i < listeners.size(); ++i)
+                            {
+                                listeners.get(i).actionPerformed(e);
+                            }
                         }
-                    }
-                });
-                rightpanel.add(bt[i]);
-            }
+                    });
+                    rightpanel.add(bt[i2]);
 
+                }
+            }
             add(leftpanel, BorderLayout.WEST);
             add(midpanel, BorderLayout.CENTER);
             add(rightpanel, BorderLayout.EAST);
             add(uppanel, BorderLayout.NORTH);
             add(downpanel, BorderLayout.SOUTH);
-        } else if (planemodel == 2) {
+        } else if (planemodel == 2)
+        {
+
 
             // here is the plane planemodel[2]
-            leftpanel.setLayout(new GridLayout(0, 4));
-            rightpanel.setLayout(new GridLayout(0, 4));
+            leftpanel.setLayout(new GridLayout(0, 5));
+            rightpanel.setLayout(new GridLayout(0, 5));
             leftpanel.setPreferredSize(new Dimension(240, 180));
             rightpanel.setPreferredSize(new Dimension(240, 180));
             // planemodel 2 (4+4)×12
@@ -171,13 +222,15 @@ public class ChooseSeatView extends JPanel {
             midpanel.add(aisle);
             midpanel.add(planemodeltext);
 
-            uppanel.setLayout(new GridLayout(0, 10)); // 10 columns
+            uppanel.setLayout(new GridLayout(0, 12)); // 10 columns
             uppanel.setPreferredSize(new Dimension(0, 30));
-            JLabel uplabel[] = new JLabel[10]; // A B C D E F G H
+            JLabel uplabel[] = new JLabel[12]; // A B C D E F G H
             int aisie = 0;
-            for (int i = 0; i < 10; i++) {
-                if (i == 4 || i == 5) {
-                    uplabel[i] = new JLabel("     ", JLabel.CENTER);
+            for (int i = 0; i < 12; i++)
+            {
+                if (i == 4 || i == 5 || i == 6 || i == 7)
+                {
+                    uplabel[i] = new JLabel("             ", JLabel.CENTER);
                     uppanel.add(uplabel[i]);
                     aisie++;
                     continue;
@@ -194,16 +247,22 @@ public class ChooseSeatView extends JPanel {
             button2 = new JButton("Next");
             button1.setForeground(Color.black);
             button2.setForeground(Color.black);
-            button1.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    for (int i = 0; i < listeners.size(); ++i) {
+            button1.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    for (int i = 0; i < listeners.size(); ++i)
+                    {
                         listeners.get(i).actionPerformed(e);
                     }
                 }
             });
-            button2.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    for (int i = 0; i < listeners.size(); ++i) {
+            button2.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    for (int i = 0; i < listeners.size(); ++i)
+                    {
                         listeners.get(i).actionPerformed(e);
                     }
                 }
@@ -213,37 +272,75 @@ public class ChooseSeatView extends JPanel {
             downpanel.add(button2);
 
             // add seat button on leftpanel and right panel
+            int i1, line1 = 0;
+            for (int index = 1; index <= 60; index++)
+            {
+                if (index % 5 == 0)
+                {
+                    line1++;
+                    JButton j = new JButton(String.valueOf(line1));
+                    leftpanel.add(j);
+                    j.setMargin(new Insets(0, 0, 0, 0));
+                    j.setEnabled(false);
+                    j.setBackground(Color.lightGray);
 
-            for (int i = 1; i <= 48; i++) {
-                if (a[i] == 0)
-                    bt[i] = new JButton(whiteicon); // 空闲位置是white
-                else
-                    bt[i] = new JButton(redicon); // 被占位置是red
+                } else
+                {
+                    i1 = index - line1;
+                    if (a[i1] == 0)
+                        bt[i1] = new JButton(whiteicon); // 空闲位置是white
+                    else
+                        bt[i1] = new JButton(redicon); // 被占位置是red
 
-                bt[i].addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        for (int i = 0; i < listeners.size(); ++i) {
-                            listeners.get(i).actionPerformed(e);
+                    bt[i1].addActionListener(new ActionListener()
+                    {
+                        public void actionPerformed(ActionEvent e)
+                        {
+                            for (int i = 0; i < listeners.size(); ++i)
+                            {
+                                listeners.get(i).actionPerformed(e);
+                            }
                         }
-                    }
-                });
-                leftpanel.add(bt[i]);
+                    });
+                    leftpanel.add(bt[i1]);
+                }
             }
-            for (int i = 49; i <= 96; i++) {
-                if (a[i] == 0)
-                    bt[i] = new JButton(whiteicon);
-                else
-                    bt[i] = new JButton(redicon);
 
-                bt[i].addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        for (int i = 0; i < listeners.size(); ++i) {
-                            listeners.get(i).actionPerformed(e);
+
+            int i2, line2 = 0;
+            for (int index = 49; index <= 108; index++)
+            {
+                if ((index + 1) % 5 == 0)
+                {
+                    line2++;
+                    JButton j = new JButton(String.valueOf(line2));
+                    rightpanel.add(j);
+                    j.setMargin(new Insets(0, 0, 0, 0));
+                    j.setEnabled(false);
+                    j.setBackground(Color.lightGray);
+
+                } else
+                {
+                    i2 = index - line2;
+                    if (a[i2] == 0)
+                        bt[i2] = new JButton(whiteicon);
+                    else
+                        bt[i2] = new JButton(redicon);
+
+                    bt[i2].addActionListener(new ActionListener()
+                    {
+                        public void actionPerformed(ActionEvent e)
+                        {
+                            for (int i = 0; i < listeners.size(); ++i)
+                            {
+                                listeners.get(i).actionPerformed(e);
+                            }
                         }
-                    }
-                });
-                rightpanel.add(bt[i]);
+                    });
+                    rightpanel.add(bt[i2]);
+                }
             }
+
             add(leftpanel, BorderLayout.WEST);
             add(midpanel, BorderLayout.CENTER);
             add(rightpanel, BorderLayout.EAST);
@@ -252,32 +349,41 @@ public class ChooseSeatView extends JPanel {
         }
     }
 
-    public boolean checkValidity() {
+    public boolean checkValidity()
+    {
 
-        if (cnt != 1) {
+        if (cnt != 1)
+        {
 
             return false;
-        } else {
+        } else
+        {
             return true;
         }
     }
 
-    public String getSelectedSeatNo(int planemodel) {
-        if (planemodel == 1) {
+    public String getSelectedSeatNo(int planemodel)
+    {
+        if (planemodel == 1)
+        {
             int selectNo = 1;
-            for (int i = 1; i <= 60; i++) {
+            for (int i = 1; i <= 60; i++)
+            {
                 if (bt[i].getIcon() == whiteicon)
                     a[i] = 0;
                 else
                     a[i] = 1;
 
-                if (bt[i].getIcon() == greenicon) {
+                if (bt[i].getIcon() == greenicon)
+                {
                     selectNo = i;
                 }
             }
 
-            if (selectNo <= 30) {
-                switch (selectNo % 3) {
+            if (selectNo <= 30)
+            {
+                switch (selectNo % 3)
+                {
                     case 1:
                         return selectNo + "A";
                     case 2:
@@ -285,8 +391,10 @@ public class ChooseSeatView extends JPanel {
                     case 0:
                         return selectNo + "C";
                 }
-            } else {
-                switch (selectNo % 3) {
+            } else
+            {
+                switch (selectNo % 3)
+                {
                     case 1:
                         return selectNo + "D";
                     case 2:
@@ -296,21 +404,26 @@ public class ChooseSeatView extends JPanel {
                 }
             }
             return "None error!";
-        } else if (planemodel == 2) {
+        } else if (planemodel == 2)
+        {
             int selectNo = 1;
-            for (int i = 1; i <= 96; i++) {
+            for (int i = 1; i <= 96; i++)
+            {
                 if (bt[i].getIcon() == whiteicon)
                     a[i] = 0;
                 else
                     a[i] = 1;
 
-                if (bt[i].getIcon() == greenicon) {
+                if (bt[i].getIcon() == greenicon)
+                {
                     selectNo = i;
                 }
             }
 
-            if (selectNo <= 48) {
-                switch (selectNo % 4) {
+            if (selectNo <= 48)
+            {
+                switch (selectNo % 4)
+                {
                     case 1:
                         return selectNo + "A";
                     case 2:
@@ -320,8 +433,10 @@ public class ChooseSeatView extends JPanel {
                     case 0:
                         return selectNo + "D";
                 }
-            } else {
-                switch (selectNo % 4) {
+            } else
+            {
+                switch (selectNo % 4)
+                {
                     case 1:
                         return selectNo + "E";
                     case 2:
@@ -333,7 +448,8 @@ public class ChooseSeatView extends JPanel {
                 }
             }
             return "None error!";
-        } else {
+        } else
+        {
             return "None error!";
         }
     }
